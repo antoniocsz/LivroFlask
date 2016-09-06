371ba25c38081e977c2e56ed9eac2ffaff11ad34
Antonio Santana <antcesar.santana@gmail.com>
2016-09-06 10:11:20 -0300

    confirmação de e-mail

--- config.py
+++ config.py
@@ -4,13 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USE_TLS = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[PyCoffee]'
-    FLASKY_MAIL_SENDER = 'Antonio Santana <antcesar.santana@gmail.com>'
+    FLASKY_MAIL_SENDER = 'Antonio Santana <antcesar.santana2@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
