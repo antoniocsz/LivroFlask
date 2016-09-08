bc7cf24e598a91c7349ef96344e32cd38dc57560
Antonio Santana <antcesar.santana@gmail.com>
2016-09-08 03:05:42 -0300

    initial parte dos posts no Blog

--- app/main/views.py
+++ app/main/views.py
@@ -2,15 +2,21 @@ from flask import render_template, redirect, abort, url_for, flash
 from flask_login import login_required, current_user
 from . import main
 from .. import db
-from .forms import EditProfileForm, EditProfileAdminForm
-from ..models import User, Role
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm
+from ..models import Permission, User, Role, Post
 from ..decorators import admin_required
 
 
 # Views
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
+        post = Post(body=form.body.data, author=current_user.get_get_current_object())
+        db.session.add(post)
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
 
 
 @main.route('/user/<username>')
