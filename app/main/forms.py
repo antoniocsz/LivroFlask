80e4147d3f53d004bb18f5d93ea78abc15bdb4d2
Antonio Santana <antcesar.santana@gmail.com>
2016-09-08 23:42:51 -0300

    PageDown

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField, SubmitField, BooleanField, SelectField, ValidationError
 from wtforms.validators import Required, Length, Email, Regexp
+from flask_pagedown.fields import PageDownField
 from ..models import Role, User
 
 
@@ -43,5 +44,5 @@ class EditProfileAdminForm(Form):
 
 
 class PostForm(Form):
-    body = TextAreaField("What's on your mind?", validators=[Required()])
+    body = PageDownField("What's on your mind?", validators=[Required()])
     submit = SubmitField('Submit')
